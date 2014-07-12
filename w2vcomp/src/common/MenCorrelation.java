d6f40fac7b86c5c495c5ae00042c40e448dab474
thenghia.pham <thenghia.pham@unitn.it>
2014-07-12 14:41:32 +0200

    men pearson spearman

--- w2vcomp/src/common/MenCorrelation.java
+++ w2vcomp/src/common/MenCorrelation.java
@@ -3,13 +3,18 @@ package common;
 import java.util.ArrayList;
 
 import org.apache.commons.math3.stat.correlation.PearsonsCorrelation;
+import org.apache.commons.math3.stat.correlation.SpearmansCorrelation;
+
+import space.SemanticSpace;
 
 public class MenCorrelation {
 	String[][] wordPairs;
 	double[] golds;
 	PearsonsCorrelation pearson;
+	SpearmansCorrelation spearman;
 	public MenCorrelation(String dataset) {
 	    pearson = new PearsonsCorrelation();
+	    spearman = new SpearmansCorrelation();
 		readDataset(dataset);
 	}
 	public void readDataset(String dataset) {
@@ -29,5 +34,22 @@ public class MenCorrelation {
 	    return pearson.correlation(golds, predicts);
 	}
 	
+	public double spearmanCorrelation(double[] predicts) {
+        return spearman.correlation(golds, predicts);
+    }
 	
+	public double evaluateSpacePearson(SemanticSpace space) {
+	    double[] predicts = new double[golds.length];
+	    for (int i = 0; i < golds.length; i++) {
+	        predicts[i] = space.getSim(wordPairs[i][0], wordPairs[i][1]);
+	    }
+	    return pearson.correlation(golds, predicts);
+	}
+	public double evaluateSpaceSpearman(SemanticSpace space) {
+        double[] predicts = new double[golds.length];
+        for (int i = 0; i < golds.length; i++) {
+            predicts[i] = space.getSim(wordPairs[i][0], wordPairs[i][1]);
+        }
+        return spearman.correlation(golds, predicts);
+    }
 }
