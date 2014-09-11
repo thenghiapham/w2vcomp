120a62e92cc14981a4a5ff856c378f2367858daf
thenghia.pham <thenghia.pham@unitn.it>
2014-09-11 17:45:48 +0200

    - plot men

--- w2vcomp/src/utils/LogPlotting.java
+++ w2vcomp/src/utils/LogPlotting.java
@@ -1,5 +1,6 @@
 package utils;
 
+import java.io.File;
 import java.util.ArrayList;
 
 import javax.swing.JFrame;
@@ -17,15 +18,19 @@ public class LogPlotting extends JFrame {
      */
     private static final long serialVersionUID = 1L;
 
-    public LogPlotting(String logFile) {
+    public LogPlotting(String logDirPath) {
         
         ArrayList<ArrayList<Double>> data = new ArrayList<>();
         ArrayList<String> labels = new ArrayList<>();
-        
-        ArrayList<Double> men = IOUtils.readLog(TestConstants.S_LOG_FILE, "men");
-        data.add(men);
-        labels.add("men");
-        
+        File logDir = new File(logDirPath);
+        String[] fileNames = logDir.list();
+        for (String fileName : fileNames) {
+            String filePath = logDirPath + "/" + fileName;
+            String suffix = fileName.split(".")[fileName.split(".").length - 1];
+            ArrayList<Double> men = IOUtils.readLog(filePath, "men");
+            data.add(men);
+            labels.add(suffix);
+        }
         XYPlot plot = PlotUtils.createTimePlot(data, labels);
         
         getContentPane().add(new InteractivePanel(plot));
@@ -36,7 +41,7 @@ public class LogPlotting extends JFrame {
     }
     
     public static void main(String args[]) {
-        LogPlotting plotFrame = new LogPlotting("");
+        LogPlotting plotFrame = new LogPlotting(TestConstants.S_LOG_DIR);
         plotFrame.setVisible(true);
     }
     
