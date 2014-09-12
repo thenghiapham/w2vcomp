f27b895b2f159c8ff67a1a06a98bbba6a0762c98
thenghia.pham <thenghia.pham@unitn.it>
2014-09-12 18:26:52 +0200

    - activated weighted additive

--- w2vcomp/src/composition/FullAdditive.java
+++ w2vcomp/src/composition/FullAdditive.java
@@ -1,18 +1,30 @@
 package composition;
 
+import neural.ActivationFunction;
+
 import org.ejml.simple.SimpleMatrix;
 
 import common.SimpleMatrixUtils;
 
 public class FullAdditive extends BasicComposition{
     protected SimpleMatrix compositionMatrix;
+    protected ActivationFunction function;
     public FullAdditive(SimpleMatrix compositionMatrix) {
         this.compositionMatrix = compositionMatrix;
     }
     
+    public FullAdditive(SimpleMatrix compositionMatrix, ActivationFunction function) {
+        this.compositionMatrix = compositionMatrix;
+        this.function = function;
+    } 
+    
     // Everything is row vectors or row major matrices
     public SimpleMatrix compose(SimpleMatrix uMatrix, SimpleMatrix vMatrix) {
-        return compositionMatrix.mult(SimpleMatrixUtils.vStack(uMatrix.transpose(), vMatrix.transpose())).transpose();
+        SimpleMatrix result = compositionMatrix.mult(SimpleMatrixUtils.vStack(uMatrix.transpose(), vMatrix.transpose())).transpose();
+        if (function != null) {
+            result = SimpleMatrixUtils.applyActivationFunction(result, function);
+        }
+        return result;
     }
     
 }
