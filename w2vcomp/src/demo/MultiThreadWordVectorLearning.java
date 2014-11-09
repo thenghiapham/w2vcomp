e7d6232d30f20cf8e25dc0b8485465b653a53e3a
thenghia.pham <thenghia.pham@unitn.it>
2014-11-09 18:39:05 +0100

    - still checking

---
+++ w2vcomp/src/demo/MultiThreadWordVectorLearning.java
@@ -0,0 +1,87 @@
+package demo;
+
+import io.sentence.PlainSentenceInputStream;
+import io.word.CombinedWordInputStream;
+import io.word.PushBackWordStream;
+import io.word.WordInputStream;
+import io.sentence.SentenceInputStream;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+
+import common.LogUtils;
+import common.exception.ValueException;
+
+import vocab.Vocab;
+import word2vec.MultiThreadMMSkipgram;
+//import word2vec.MultiThreadSkipGram;
+//import word2vec.CBowWord2Vec;
+
+import demo.TestConstants;
+
+public class MultiThreadWordVectorLearning {
+    public static void main(String[] args) throws ValueException, IOException {
+//        MmSkipNGramWithMappingCosine word2vec = new MmSkipNGramWithMappingCosine(300, 5, true, 0,1, (float) 1e-3, TestConstants.CCG_MEN_FILE);
+        MultiThreadMMSkipgram word2vec = new MultiThreadMMSkipgram(300, 5, true, 0,-1, (float) 1e-3, TestConstants.MEN_FILE);
+
+        //MmSkipNGramWithMappingDot word2vec = new MmSkipNGramWithMappingDot(300, 5, true, 0, 1, (float) 1e-3, TestConstants.CCG_MEN_FILE);
+        //MMSkipNgramWord2Vec word2vec = new MMSkipNgramWord2Vec(300, 5, true, 0, 20, (float) 1e-3, TestConstants.CCG_MEN_FILE);
+        
+        
+        //TODO: Assume that we extend vocabulary with new items
+        //TODO: Assume that for every word we have its extended context
+        String trainDirPath = TestConstants.TRAIN_DIR;
+        String outputFile = TestConstants.VECTOR_FILE;
+        String vocabFile = TestConstants.VOCABULARY_FILE;
+        String initFile = TestConstants.INITIALIZATION_FILE;
+        String logGile = TestConstants.LOG_FILE;
+        String mapFile = TestConstants.MAPPING_FUNCTION;
+        LogUtils.setup(logGile);
+        File trainDir = new File(trainDirPath);
+        File[] trainFiles = trainDir.listFiles();
+        
+        boolean learnVocab = !(new File(vocabFile)).exists();
+        
+        
+        Vocab vocab = new Vocab(5); //wiki 50, enwik9 5
+        if (!learnVocab)
+            vocab.loadVocab(vocabFile);// ,minFrequency);
+        else {
+            ArrayList<WordInputStream> wordStreamList = new ArrayList<>();
+            for (File trainFile: trainFiles) {
+                PushBackWordStream wordStream = new PushBackWordStream(trainFile.getAbsolutePath(), 100);
+                wordStreamList.add(wordStream);
+            }
+            
+            CombinedWordInputStream wordStream = new CombinedWordInputStream(wordStreamList);
+            vocab.learnVocabFromTrainStream(wordStream);
+            // save vocabulary
+            vocab.saveVocab(vocabFile);
+        }
+
+        word2vec.setVocab(vocab);
+        word2vec.initNetwork();
+//        word2vec.initNetwork(initFile);
+        //word2vec.saveMappingFunction(mapFile, false);
+        
+        word2vec.initImages(TestConstants.VISION_FILE,true);
+        
+        // single threaded instead of multithreading
+        System.out.println("Start training");
+        try {
+            ArrayList<SentenceInputStream> inputStreams = new ArrayList<SentenceInputStream>();
+            for (File trainFile: trainFiles) {
+                SentenceInputStream sentenceInputStream = new PlainSentenceInputStream(
+                        new PushBackWordStream(trainFile.getAbsolutePath(), 100));
+                inputStreams.add(sentenceInputStream);
+            }
+            word2vec.trainModel(inputStreams);
+            word2vec.saveVector(outputFile, true);
+        } catch (IOException e) {
+            System.exit(1);
+        }
+       
+    }
+}
--- w2vcomp/src/demo/TestConstants.java
+++ w2vcomp/src/demo/TestConstants.java
@@ -2,55 +2,39 @@ package demo;
 
 public class TestConstants {
 
-    // public static final String TRAIN_FILE = "/home/pham/svn/word2vec/text8";
-    // public static final String VECTOR_FILE =
-    // "/home/pham/svn/word2vec/out.bin";
-    // public static final String VOCABULARY_FILE =
-    // "/home/pham/svn/word2vec/out.voc";
-    // public static final String INITIALIZATION_FILE =
-    // "/home/pham/svn/word2vec/out.ini";
-
-    // public static final String GZIP_TRAIN_FILES =
-    // "/home/pham/svn/word2vec/bnc.utf8.xml.gz;"
-    // + "/home/pham/svn/word2vec/wikipedia_sample.xml.gz";
-    // public static final String GZIP_VECTOR_FILE =
-    // "/home/pham/svn/word2vec/malt.bin";
-    // public static final String GZIP_VOCABULARY_FILE =
-    // "/home/pham/svn/word2vec/malt.voc";
-    // public static final String GZIP_INITIALIZATION_FILE =
-    // "/home/pham/svn/word2vec/malt.ini";
 
     public static final String TRAIN_FILE               = "/home/angeliki/Documents/mikolov_composition/corpora_simple/enwik9";
-    //public static final String TRAIN_FILE               = "/home/angeliki/masterclic4/mikolov_composition/corpora/wiki.sentences";
-   //public static final String VISION_FILE_IMAGES         = "/home/angeliki/Documents/mikolov_composition/misc/pmisvd.dm";
     public static final String VISION_FILE              = "/home/angeliki/Documents/mikolov_composition/misc/pmisvd.aggr.dm";
-    //public static final String VISION_FILE                =  "/home/angeliki/Documents/mikolov_composition/misc/cnn.aggr";
     public static final long SEED                       = 292626718599866L;
     public static final String TRAIN_CONCEPTS              = "/home/angeliki/Documents/mikolov_composition/misc/trainingConcepts.txt";                                     
     public static final String VECTOR_FILE              = "/home/angeliki/Documents/mikolov_composition/out/multimodal/hierarchical_stochastic_mapping/out_enwiki9_1_r4cosl0001.bin";
     public static final String VOCABULARY_FILE          = "/home/angeliki/Documents/mikolov_composition/out/multimodal/hierarchical_stochastic/out_enwik9.voc";
     public static final String INITIALIZATION_FILE      = "/home/angeliki/Documents/mikolov_composition/out/multimodal/hierarchical_stochastic/out_enwik9.ini";
-    //public static final String LOG_FILE               = "/home/angeliki/workspace/w2vcomp/w2vcomp/logs/multi_enwiki8_5_rand2.log";
     public static final String LOG_FILE               = "/home/angeliki/workspace/w2vcomp/w2vcomp/logs/dump.log";
     public static final String MAPPING_FUNCTION         = "/home/angeliki/Documents/mikolov_composition/out/multimodal/hierarchical_stochastic_mapping/out_enwiki9_1_r4cosl0001.txt";         
-
     public static final String LOG_DIR               = "/home/angeliki/workspace/w2vcomp/w2vcomp/logs";
+    
+    public static final String TRAIN_DIR               = "";
+    public static final String MEN_FILE             = "/home/angeliki/Documents/mikolov_composition/misc/MEN_dataset_lemma_nopos_form_full";
+//    public static final String PROJECT_DIR              = "/home/thenghiapham/work/project/multimik";
+//    public static final String TRAIN_DIR               = PROJECT_DIR + "/corpus/split/enwik9";
+//    public static final String TRAIN_FILE               = PROJECT_DIR + "/corpus/enwik9";
+//    public static final String VISION_FILE              = PROJECT_DIR + "/misc/pmisvd.aggr.dm";
+//    public static final long SEED                       = 292626718599866L;
+//    public static final String TRAIN_CONCEPTS           = PROJECT_DIR + "/misc/trainingConcepts.txt";                                     
+//    public static final String VECTOR_FILE              = PROJECT_DIR + "/output/wiki9.bin";
+//    public static final String VOCABULARY_FILE          = PROJECT_DIR + "/output/wiki9.voc";
+//    public static final String INITIALIZATION_FILE      = PROJECT_DIR + "/output/wiki9.ini";
+//    public static final String MEN_FILE                 = PROJECT_DIR + "/men/MEN_dataset_lemma.txt";
+//    public static final String LOG_FILE                 = PROJECT_DIR + "/output/wiki9.log";
+//    public static final String MAPPING_FUNCTION         = PROJECT_DIR + "/output/wiki9.map.txt";         
+//    public static final String LOG_DIR                  = PROJECT_DIR + "/log";
 
     
     public static final double threshold = 1;
     public static final int imageDimensions = 300;
     public static final double rate_multiplier = 4;
     
-    // public static final String GZIP_TRAIN_FILES =
-    // "/home/thenghiapham/svn/word2vec/bnc.utf8.xml.gz";
-    // // public static final String GZIP_TRAIN_FILE =
-    // "/home/thenghiapham/svn/word2vec/wikipedia_sample.xml.gz";we
-    // public static final String GZIP_VECTOR_FILE =
-    // "/home/thenghiapham/svn/word2vec/malt.bin";
-    // public static final String GZIP_VOCABULARY_FILE =
-    // "/home/thenghiapham/svn/word2vec/malt.voc";
-    // public static final String GZIP_INITIALIZATION_FILE =
-    // "/home/thenghiapham/svn/word2vec/malt.ini";
 
     // on the cluster
     public static final String PATH                     = "/mnt/cimec-storage-sata/users/marco.baroni/share/ukwac-maltparsing/data/";
