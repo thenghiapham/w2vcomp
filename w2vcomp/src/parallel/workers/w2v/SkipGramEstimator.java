d94e6d67ed5616af7e6700e6a23f646afb5d46e4
thenghia.pham <thenghia.pham@unitn.it>
2014-11-21 17:22:30 +0100

    aa

--- w2vcomp/src/parallel/workers/w2v/SkipGramEstimator.java
+++ w2vcomp/src/parallel/workers/w2v/SkipGramEstimator.java
@@ -84,7 +84,7 @@ public class SkipGramEstimator implements ParameterEstimator{
         long oldWordCount = 0;
         long wordCount = 0;
         try {
-            double mean_batch = 1.0/2000000.0;
+            double mean_batch = 1.0/500000.0;
             double batch_size = Math.log(1 - rand.nextDouble()) / -mean_batch;
             while (true) {
 
