e601a8dcc9631dafc79f662c568e36ab899e5178
thenghia.pham <thenghia.pham@unitn.it>
2014-12-01 20:05:02 +0100

    - what happened

--- w2vcomp/src/word2vec/Paragraph2Vec.java
+++ w2vcomp/src/word2vec/Paragraph2Vec.java
@@ -8,12 +8,14 @@ import common.exception.ValueException;
 
 public class Paragraph2Vec extends AbstractWord2Vec{
     protected double[][] paragraphVectors;
+    protected int iteration;
     
     public Paragraph2Vec(String networkFile, int projectionLayerSize, int windowSize,
-            boolean hierarchicalSoftmax, int negativeSamples, double subSample) {
+            boolean hierarchicalSoftmax, int negativeSamples, double subSample, int iteration) {
         super(projectionLayerSize, windowSize, hierarchicalSoftmax, negativeSamples,
                 subSample);
         loadNetwork(networkFile, true);
+        this.iteration = iteration;
     }
 
     public void trainParagraphVector(String[] sentences) {
