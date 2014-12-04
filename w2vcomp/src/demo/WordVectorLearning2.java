9486455f264f36cc509cc7c37ee708316e209cba
pham <phamthenghia1985@gmail.com>
2014-12-04 11:54:43 +0100

    - 300

--- w2vcomp/src/demo/WordVectorLearning2.java
+++ w2vcomp/src/demo/WordVectorLearning2.java
@@ -21,7 +21,7 @@ import demo.TestConstants;
 public class WordVectorLearning2 {
     public static void main(String[] args) throws IOException{
 //        MultiThreadSkipGram word2vec = new MultiThreadSkipGram(100, 5, true, 0, 0, TestConstants.S_MEN_FILE);
-        CBowWord2Vec word2vec = new CBowWord2Vec(100, 5, true, 0, 0, TestConstants.S_MEN_FILE);
+        CBowWord2Vec word2vec = new CBowWord2Vec(300, 5, true, 0, 0, TestConstants.S_MEN_FILE);
         String trainDirPath = TestConstants.S_TRAIN_DIR;
         String outputFile = TestConstants.S_VECTOR_FILE;
         String vocabFile = TestConstants.S_VOCABULARY_FILE;
