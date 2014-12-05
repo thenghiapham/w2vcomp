2f56af4ded960994eefb785766ba2bafef5e8696
pham <phamthenghia1985@gmail.com>
2014-12-05 09:11:41 +0100

    wohoo

---
+++ w2vcomp/src/utils/Tree2String.java
@@ -0,0 +1,72 @@
+package utils;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import demo.TestConstants;
+import tree.Tree;
+import io.sentence.BasicTreeInputStream;
+
+public class Tree2String {
+    public static void printArray2Stream(String[] sentence, BufferedWriter outWriter) throws IOException {
+        int length = sentence.length;
+        for (int i = 0; i < length - 1; i++) {
+            outWriter.write(sentence[i]);
+            outWriter.write(" ");
+        }
+        if (length > 0) {
+            outWriter.write(sentence[length - 1]);
+        }
+        outWriter.write("\n");
+    }
+    
+    public static void treeStream2StringStream(BasicTreeInputStream inStream, BufferedWriter outWriter) throws IOException {
+        Tree tree;
+        tree = inStream.readTree();
+        while (tree != null) {
+            String[] sentence = tree.getSurfaceWords();
+            printArray2Stream(sentence, outWriter);
+            tree = inStream.readTree();
+        }
+                
+    }
+    
+    public static void treeFile2StringFile(String inFile, String outFile) throws IOException {
+        BasicTreeInputStream inStream = new BasicTreeInputStream(inFile);
+        BufferedWriter outWriter = new BufferedWriter(new FileWriter(outFile));
+        treeStream2StringStream(inStream, outWriter);
+        inStream.close();
+        outWriter.close();
+    }
+    
+    public static void treeDir2StringDir(String srcDir, String destDir) throws IOException {
+        File dir = new File(srcDir);
+        File[] files = dir.listFiles();
+        for (File file: files) {
+            String inFileName = file.getName();
+            String outFile = destDir + "/" + inFileName;
+            treeFile2StringFile(file.getAbsolutePath(), outFile);
+        }
+    }
+    
+    public static void treeDir2StringFile(String inDir, String outFile) throws IOException {
+        File dir = new File(inDir);
+        File[] files = dir.listFiles();
+        BufferedWriter outWriter = new BufferedWriter(new FileWriter(outFile));
+
+        for (File file: files) {
+            BasicTreeInputStream inStream = new BasicTreeInputStream(file.getAbsolutePath());
+            treeStream2StringStream(inStream, outWriter);
+            inStream.close();
+        }
+        outWriter.close();
+    }
+    
+    public static void main(String[] args) throws IOException {
+        String inDir = TestConstants.S_TRAIN_DIR;
+        String outFile = "/mnt/cimec-storage-sata/users/thenghia.pham/data/project/mikcom/corpus/rawWiki.txt";
+        treeDir2StringFile(inDir, outFile);
+    }
+}
