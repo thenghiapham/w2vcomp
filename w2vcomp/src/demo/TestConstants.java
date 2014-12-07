4cf4dbfa36274ea18162aa8dafbc16f6ec8c617b
pham <phamthenghia1985@gmail.com>
2014-12-07 15:12:14 +0100

    - bnc 40

--- w2vcomp/src/demo/TestConstants.java
+++ w2vcomp/src/demo/TestConstants.java
@@ -40,10 +40,10 @@ public class TestConstants {
 //    public static final String S_VOCABULARY_FILE          = S_OUT_DIR + "wbnc.voc";
     public static final String S_INITIALIZATION_FILE      = S_OUT_DIR + "skipgram.mdl";
     
-    public static final String S_VECTOR_FILE              = S_OUT_DIR + "skip_bnc300r.bin";
-    public static final String S_COMPOSITION_FILE         = S_OUT_DIR + "skip_bnc300r.cmp";
+    public static final String S_VECTOR_FILE              = S_OUT_DIR + "skip_bnc40.bin";
+    public static final String S_COMPOSITION_FILE         = S_OUT_DIR + "skip_bnc40.cmp";
     public static final String S_VOCABULARY_FILE          = S_OUT_DIR + "skip_bnc.voc";
-    public static final String S_MODEL_FILE               = S_OUT_DIR + "skip_bnc300r.mdl";
+    public static final String S_MODEL_FILE               = S_OUT_DIR + "skip_bnc40.mdl";
     
 //    public static final String S_VECTOR_FILE              = S_OUT_DIR + "cbow_bnc100.bin";
 //    public static final String S_COMPOSITION_FILE         = S_OUT_DIR + "cbow_bnc100.cmp";
