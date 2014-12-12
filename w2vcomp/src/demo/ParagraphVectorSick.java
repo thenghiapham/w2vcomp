e579000609dcd805e59d8c3a92a1f146265f70a8
thenghia.pham <thenghia.pham@unitn.it>
2014-12-12 18:56:34 +0100

    - boo

--- w2vcomp/src/demo/ParagraphVectorSick.java
+++ w2vcomp/src/demo/ParagraphVectorSick.java
@@ -9,15 +9,17 @@ import common.correlation.ParsedPhraseCorrelation;
 public class ParagraphVectorSick {
     public static void main(String[] args) {
         String modelFile = TestConstants.S_MODEL_FILE;
+        int vecSize = 100;
         if (args.length > 0) {
             modelFile = args[0];
+            vecSize = Integer.parseInt(args[1]);
         }
         String vocabFile = TestConstants.S_VOCABULARY_FILE;
         String sickFile  = TestConstants.S_SICK_FILE;
         ParsedPhraseCorrelation sick = new ParsedPhraseCorrelation(sickFile);
         String[] sentences = sick.getSurfacePhrase();
 //        Paragraph2Vec p2v = new CBowPara2Vec(modelFile, vocabFile, 100, 5, true, 0, 0, 20);
-        Paragraph2Vec p2v = new SkipgramPara2Vec(modelFile, vocabFile, 100, 5, true, 0, 0, 100);
+        Paragraph2Vec p2v = new SkipgramPara2Vec(modelFile, vocabFile, vecSize, 5, true, 0, 0, 100);
         p2v.trainParagraphVector(sentences);
         double[][] sentenceVector = p2v.getParagraphVectors();
         RawSemanticSpace space = new RawSemanticSpace(sentences, sentenceVector);
