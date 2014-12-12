120ac1a9c34d54e81406a82be5c732870f217f80
pham <phamthenghia1985@gmail.com>
2014-12-12 02:34:38 +0100

    - true

--- w2vcomp/src/demo/TestConstants.java
+++ w2vcomp/src/demo/TestConstants.java
@@ -101,11 +101,11 @@ public class TestConstants {
 //    public static final String S_VOCABULARY_FILE          = S_OUT_DIR + "bnc.voc";
 //    public static final String S_INITIALIZATION_FILE      = S_OUT_DIR + "bnc.ini";
     
-    public static final String S_VECTOR_FILE              = S_OUT_DIR + "i_dwiki100is.bin";
-    public static final String S_COMPOSITION_FILE         = S_OUT_DIR + "i_dwiki100is.cmp";
-    public static final String S_MODEL_FILE               = S_OUT_DIR + "i_dwiki100is.mdl";
-    public static final String S_INITIALIZATION_FILE      = S_OUT_DIR + "i_dwiki100is.ini";
-    public static final String S_LOG_FILE                 = S_LOG_DIR + "i_dwiki100is.log";
+    public static final String S_VECTOR_FILE              = S_OUT_DIR + "i_dwiki300is.bin";
+    public static final String S_COMPOSITION_FILE         = S_OUT_DIR + "i_dwiki300is.cmp";
+    public static final String S_MODEL_FILE               = S_OUT_DIR + "i_dwiki300is.mdl";
+    public static final String S_INITIALIZATION_FILE      = S_OUT_DIR + "i_dwiki300is.ini";
+    public static final String S_LOG_FILE                 = S_LOG_DIR + "i_dwiki300is.log";
     public static final String S_VOCABULARY_FILE          = S_OUT_DIR + "neg_wiki40is.voc";
     
 //    public static final String S_VECTOR_FILE              = S_OUT_DIR + "skip_wiki300_ss.bin";
