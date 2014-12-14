5687d2f3f863883bc968e81b70531834ee4f446f
pham <phamthenghia1985@gmail.com>
2014-12-14 23:53:12 +0100

    - I don't wanna die

--- w2vcomp/src/utils/SvmCosinePrinter.java
+++ w2vcomp/src/utils/SvmCosinePrinter.java
@@ -5,11 +5,12 @@ import java.io.IOException;
 
 import common.ParsedPhraseCosinePrinter;
 import composition.WeightedAdditive;
-
 import space.CompositionSemanticSpace;
 import space.DiagonalCompositionSemanticSpace;
 import space.RawSemanticSpace;
 import space.WeightedCompositionSemanticSpace;
+import word2vec.Paragraph2Vec;
+import word2vec.SkipgramPara2Vec;
 import demo.TestConstants;
 
 public class SvmCosinePrinter {
@@ -19,26 +20,43 @@ public class SvmCosinePrinter {
         String datasetName = new File(datasetFile).getName();
         String outDir = TestConstants.S_RTE_SVM_DIR;
         ParsedPhraseCosinePrinter printer = new ParsedPhraseCosinePrinter(datasetFile, rteFeatureFile);
-        if (args.length == 1) {
-            String vectorFile = args[0];
+        String compType = args[0];
+        
+        String vectorFile = args[1];
+        String compFile = args[1];
+        String modelFile = args[1];
+        if ("w".equals(compType)) {
+            printWeighted(printer, compFile, outDir, datasetName);
+        } else if ("d".equals(compType)) {
+            printDiagonal(printer, compFile, outDir, datasetName);
+        } else if ("f".equals(compType)) {
+            printFull(printer, compFile, outDir, datasetName);
+        } else if ("a".equals(compType)) {
             printAdd(printer, vectorFile, outDir, datasetName);
-                
-        } else if (args.length == 2) {
-            String compFile = args[0];
-            String compType = args[1];
-            if ("w".equals(compType)) {
-                printWeighted(printer, compFile, outDir, datasetName);
-            } else if ("d".equals(compType)) {
-                printDiagonal(printer, compFile, outDir, datasetName);
-            } else if ("f".equals(compType)) {
-                printFull(printer, compFile, outDir, datasetName);
-            }
+        } else if ("p".equals(compType)) {
+            String vocabFile = args[2];
+            int vectorSize = Integer.parseInt(args[3]);
+            boolean hs = Boolean.parseBoolean(args[4]);
+            int neg = Integer.parseInt(args[5]);
+            double subSample = Double.parseDouble(args[6]);
+            printParagraph(printer, modelFile, vocabFile, vectorSize, hs, neg, subSample, outDir, datasetName);
         }
     }
     
     public static void printAdd(ParsedPhraseCosinePrinter printer, String vectorFile, String outDir, String suffix) throws IOException {
         RawSemanticSpace space = RawSemanticSpace.readSpace(vectorFile);
         String outFilePath = outDir + "add" + suffix;
+        printer.printCosines(outFilePath, space, new WeightedAdditive());
+    }
+    
+    public static void printParagraph(ParsedPhraseCosinePrinter printer, String modelFile, String vocabFile, 
+            int vectorSize, boolean hs, int negSample, double subSample, String outDir, String suffix) throws IOException {
+        Paragraph2Vec p2v = new SkipgramPara2Vec(modelFile, vocabFile, vectorSize, 5, hs, negSample, subSample, 100);
+        String[] sentences = printer.getSurfacePhrases();
+        p2v.trainParagraphVector(sentences);
+        double[][] sentenceVector = p2v.getParagraphVectors();
+        RawSemanticSpace space = new RawSemanticSpace(sentences, sentenceVector);
+        String outFilePath = outDir + "para" + suffix;
         printer.printCosines(outFilePath, space, new WeightedAdditive());
     }
     
