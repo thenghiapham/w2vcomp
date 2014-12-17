600a7cb17064ba4c25bde369070398cd356a6f76
thenghia.pham <thenghia.pham@unitn.it>
2014-12-17 16:08:37 +0100

    - bnc

--- w2vcomp/src/parallel/workers/w2v/RunningConstant.java
+++ w2vcomp/src/parallel/workers/w2v/RunningConstant.java
@@ -4,8 +4,8 @@ public class RunningConstant {
     public static final double STARTING_ALPHA = 0.025;
     public static final boolean HIERARCHICAL_SOFTMAX = true;
     public static final int NEGATIVE_SAMPLES = 0; 
-    public static final double SUBSAMPLE = 0;
-    public static final int VECTOR_SIZE = 100;
+    public static final double SUBSAMPLE = 1e-3;
+    public static final int VECTOR_SIZE = 40;
     public static final int WINDOW_SIZE = 5;
-    public static final int MIN_FREQUENCY = 50;
+    public static final int MIN_FREQUENCY = 5;
 }
