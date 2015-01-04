a934ba866f4e47bc1d40ef1f1e21540dc3cea0fb
pham <phamthenghia1985@gmail.com>
2015-01-04 19:35:39 +0100

    - boo hoo

--- w2vcomp/src/demo/MultiThreadSentenceVectorLearning.java
+++ w2vcomp/src/demo/MultiThreadSentenceVectorLearning.java
@@ -33,36 +33,63 @@ public class MultiThreadSentenceVectorLearning {
         int hiddenLayerSize = Integer.parseInt(args[0]);
         int windowSize = Integer.parseInt(args[1]);
         int incrementalStep = Integer.parseInt(args[2]);
+        String type = args[3];
+        String function = args[4];
+        
         boolean hierarchialSoftmax = false;
         int negativeSampling = 10;
         double subSampling = 1e-3;
         int phraseLevel = 6;
         boolean allLevel = true;
         boolean lexical = false;
+        
         String constructionFile = TestConstants.S_CONSTRUCTION_FILE;
-        ActivationFunction hiddenActivationFunction = new IdentityFunction();
-//        ActivationFunction hiddenActivationFunction = new Tanh();
-//        HashMap<String, String> constructionGroups = new HashMap<String, String>();
+//      HashMap<String, String> constructionGroups = new HashMap<String, String>();
         HashMap<String, String> constructionGroups = IOUtils.readConstructionGroup(constructionFile);
-//        MTWeightedSingleObjectSentence2Vec sentence2vec = new MTWeightedSingleObjectSentence2Vec(hiddenLayerSize, windowSize, 
-//                hierarchialSoftmax, negativeSampling, subSampling, constructionGroups, hiddenActivationFunction, phraseLevel, 
-//                allLevel, lexical);
-//        MTIncrementalWeightedSentence2Vec sentence2vec = new MTIncrementalWeightedSentence2Vec(hiddenLayerSize, windowSize, 
-//                hierarchialSoftmax, negativeSampling, subSampling, constructionGroups, hiddenActivationFunction, phraseLevel, 
-//                allLevel, lexical, incrementalStep);
-//        MTIncrementalSentence2Vec sentence2vec = new MTIncrementalSentence2Vec(hiddenLayerSize, windowSize, 
-//                hierarchialSoftmax, negativeSampling, subSampling, constructionGroups, hiddenActivationFunction, phraseLevel, 
-//                allLevel, lexical);
-        MTIncrementalDiagonalSentence2Vec sentence2vec = new MTIncrementalDiagonalSentence2Vec(hiddenLayerSize, windowSize, 
-                hierarchialSoftmax, negativeSampling, subSampling, constructionGroups, hiddenActivationFunction, phraseLevel, 
-                allLevel, lexical, incrementalStep);
+        
+        ActivationFunction hiddenActivationFunction = null;
+        if (function.equals("t")) {
+            hiddenActivationFunction = new Tanh();
+        } else {
+            function = "i";
+            hiddenActivationFunction = new IdentityFunction();
+        }
+
+        MTIncrementalSentence2Vec sentence2vec = null;
+        switch (type) {
+        case "w":
+            sentence2vec = new MTIncrementalWeightedSentence2Vec(hiddenLayerSize, windowSize, 
+                  hierarchialSoftmax, negativeSampling, subSampling, constructionGroups, hiddenActivationFunction, phraseLevel, 
+                  allLevel, lexical, incrementalStep);
+            break;
+        case "d":
+            sentence2vec = new MTIncrementalDiagonalSentence2Vec(hiddenLayerSize, windowSize, 
+                    hierarchialSoftmax, negativeSampling, subSampling, constructionGroups, hiddenActivationFunction, phraseLevel, 
+                    allLevel, lexical, incrementalStep);
+            break;
+        case "f":
+            sentence2vec = new MTIncrementalSentence2Vec(hiddenLayerSize, windowSize, 
+                  hierarchialSoftmax, negativeSampling, subSampling, constructionGroups, hiddenActivationFunction, phraseLevel, 
+                  allLevel, lexical, incrementalStep);
+            break;
+        default:
+            System.out.println("Invalid choice");
+            System.exit(0);
+            break;
+        }
         
         String trainDirPath = TestConstants.S_TRAIN_DIR;
-        String outputFile = TestConstants.S_VECTOR_FILE.replace("istep", ""+incrementalStep).replace("size", ""+hiddenLayerSize).replace("window", ""+windowSize);
-        String compFile = TestConstants.S_COMPOSITION_FILE.replace("istep", ""+incrementalStep).replace("size", ""+hiddenLayerSize).replace("window", ""+windowSize);
+        String outputFile = TestConstants.S_VECTOR_FILE.replace("istep", ""+incrementalStep)
+                .replace("size", ""+hiddenLayerSize)
+                .replace("window", ""+windowSize).replace("function", function)
+                .replace("type", type);
+        
+        String compFile = outputFile.replace(".bin",".cmp");
         String vocabFile = TestConstants.S_VOCABULARY_FILE;
-        String logFile = TestConstants.S_LOG_FILE.replace("istep", ""+incrementalStep).replace("size", ""+hiddenLayerSize).replace("window", ""+windowSize);;
-//        String skipMdlFile = TestConstants.S_INITIALIZATION_FILE.replace(".mdl", "" + hiddenLayerSize + ".mdl");
+        String logFile = TestConstants.S_LOG_FILE.replace("istep", ""+incrementalStep)
+                .replace("size", ""+hiddenLayerSize)
+                .replace("window", ""+windowSize).replace("function", function)
+                .replace("type", type);
         LogUtils.setup(logFile);
         
         File trainDir = new File(trainDirPath);
@@ -87,7 +114,6 @@ public class MultiThreadSentenceVectorLearning {
 
         sentence2vec.setVocab(vocab);
         sentence2vec.initNetwork();
-//        sentence2vec.initNetwork(skipMdlFile);
         MenCorrelation men = new MenCorrelation(TestConstants.S_MEN_FILE);
         men.setName("MEN");
         sentence2vec.addMenCorrelation(men);
@@ -96,24 +122,6 @@ public class MultiThreadSentenceVectorLearning {
         sick.setName("SICK");
         sentence2vec.addSentenceCorrelation(sick);
         
-//        ArrayList<Tree> validationTrees = IOUtils.readTree(TestConstants.S_VALIDATION_FILE);
-//        sentence2vec.setTestTrees(validationTrees);
-        // single threaded instead of multithreading
-//        System.out.println("Start training");
-//        try {
-//            
-//            ArrayList<TreeInputStream> inputStreams = new ArrayList<TreeInputStream>();
-//            for (File trainFile: trainFiles) {
-//                TreeInputStream treeInputStream = new BasicTreeInputStream(trainFile);;
-//                inputStreams.add(treeInputStream);
-//            }
-//            System.out.println(inputStreams.size());
-//            sentence2vec.trainModel(inputStreams);
-////            sentence2vec.saveVector(outputFile, true);
-////            sentence2vec.saveCompositionNetwork(compFile, true);
-//        } catch (IOException e) {
-//            System.exit(1);
-//        }
         
         System.out.println("train again");
         try {
