f978ee5a3096bc57811a80d9e23c54042a9d7a80
The Nghia Pham <phamthenghia1985@gmail.com>
2015-02-27 21:03:01 +0700

    - commit

--- w2vcomp/src/utils/PhraseEvaluation.java
+++ w2vcomp/src/utils/PhraseEvaluation.java
@@ -81,7 +81,8 @@ public class PhraseEvaluation {
                 double[][] trainData = SvmTrainTest.extractData(features, trainIndices);
                 double[][] testData = SvmTrainTest.extractData(features, testIndices);
                 SvmTrainTest trainTest = new SvmTrainTest(SVM_DIR);
-                System.out.println(name + ": " + trainTest.trainTest(trainLabels, trainData, testLabels, testData, "-t 1 -d 2 -c 20 -p 0.5 -g 0.001"));
+                System.out.println(name + ": " + trainTest.trainTest(trainLabels, trainData, testLabels, testData, ""));
+//                System.out.println(name + ": " + trainTest.trainTest(trainLabels, trainData, testLabels, testData, "-t 1 -d 2 -c 20 -p 0.5 -g 0.001"));
             }
         }
     }
