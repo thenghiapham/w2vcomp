910863caf467f026b1deff57bdee58c0ea92e34b
aggelikiL <aggeliki.lazaridou@gmail.com>
2015-03-10 10:27:40 +0100

    adding to crossSituational the non-parallel MMSkigramMaxMargin

--- w2vcomp/src/demo/WordVectorLearning.java
+++ w2vcomp/src/demo/WordVectorLearning.java
@@ -14,6 +14,7 @@ import common.exception.ValueException;
 
 import vocab.Vocab;
 import word2vec.MMSkipNgramWord2Vec;
+import word2vec.MMSkipgramMaxMargin;
 import word2vec.MmSkipNGramWithMappingMaxMargin;
 //import word2vec.CBowWord2Vec;
 import word2vec.SkipNGramWord2Vec;
@@ -23,11 +24,11 @@ import demo.TestConstants;
 
 public class WordVectorLearning {
     public static void main(String[] args) throws ValueException, IOException {
-        //MmSkipNGramWithMappingCosine word2vec = new MmSkipNGramWithMappingCosine(TestConstants.wordDimensions, 5, true, 0,TestConstants.negative_samples, (float) 1e-3, TestConstants.CCG_MEN_FILE);
         //MmSkipNGramWithMappingMaxMargin word2vec = new MmSkipNGramWithMappingMaxMargin(TestConstants.wordDimensions, 5, true, 0,TestConstants.negative_samples, (float) 1e-3);
+        
+        MMSkipgramMaxMargin word2vec = new MMSkipgramMaxMargin(TestConstants.wordDimensions, 5, true, 0,TestConstants.negative_samples, (float) 1e-3);
 
-        //MmSkipNGramWithMappingDot word2vec = new MmSkipNGramWithMappingDot(300, 5, true, 0, 1, (float) 1e-3, TestConstants.CCG_MEN_FILE);
-        MMSkipNgramWord2Vec word2vec = new MMSkipNgramWord2Vec(TestConstants.wordDimensions, 5, true, 0, TestConstants.negative_samples, (float) 1e-3);
+        //MMSkipNgramWord2Vec word2vec = new MMSkipNgramWord2Vec(TestConstants.wordDimensions, 5, true, 0, TestConstants.negative_samples, (float) 1e-3);
         
         
         //TODO: Assume that we extend vocabulary with new items
@@ -38,7 +39,6 @@ public class WordVectorLearning {
         String initFile = TestConstants.INITIALIZATION_FILE;
         String projInitFile = TestConstants.IMAGE_INITIALIZATION_FILE;
         String logGile = TestConstants.LOG_FILE;
-        String mapFile = TestConstants.MAPPING_FUNCTION;
         LogUtils.setup(logGile);
         
         System.out.println("Starting training using file " + trainFile);
@@ -48,9 +48,9 @@ public class WordVectorLearning {
 
         boolean learnVocab = !(new File(vocabFile)).exists();
         
-        Vocab vocab = new Vocab(5); //wiki 50, enwik9 5
+        Vocab vocab = new Vocab(5); 
         if (!learnVocab)
-            vocab.loadVocab(vocabFile);// ,minFrequency,5);
+            vocab.loadVocab(vocabFile);
         else {
             vocab.learnVocabFromTrainFile(trainFile);
             // save vocabulary
@@ -60,7 +60,6 @@ public class WordVectorLearning {
         word2vec.setVocab(vocab);
         
         word2vec.initNetwork(initFile,projInitFile);
-        word2vec.saveMappingFunction(mapFile, false);
         
         word2vec.initImages(TestConstants.VISION_FILE,false);
         
@@ -76,7 +75,6 @@ public class WordVectorLearning {
             inputStreams.add(sentenceInputStream);
             word2vec.trainModel(inputStreams);
             word2vec.saveVector(outputFile, true);
-            word2vec.saveMappingFunction(mapFile, false);
            } catch (IOException e) {
             System.exit(1);
         }
