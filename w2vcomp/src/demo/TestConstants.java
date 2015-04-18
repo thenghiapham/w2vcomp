45def4b46b66a4df05efce28ba2c9c7b97af2c1f
Angeliki Lazaridou <aggeliki.lazaridou@gmail.com>
2015-04-18 18:20:04 +0200

    changing paths

--- w2vcomp/src/demo/TestConstants.java
+++ w2vcomp/src/demo/TestConstants.java
@@ -15,44 +15,54 @@ public class TestConstants {
         public static final double threshold = 5;
         public static final double margin = 0.5;
     
-    
+        public static final long SEED                       = 292626718599866L;
+
+        
+        public static final String ROOT_EXP_DIR                     = "/home/angeliki/Documents/cross-situational/";
+        public static final String ROOT_EVAL_DIR        = "/home/angeliki/Documents/mikolov_composition/misc/";
+        public static final String ROOT_VISUAL_DIR        = "/home/angeliki/sas/visLang/cross-situational/visual_symbols/all/";
+
+
+        
         
         //for pc
-        public static final String SOURCE_FILE               = "/home/angeliki/Documents/cross-situational/corpus/test_version/cds.11_24.words.txt";
-        public static final String TARGET_FILE              =  "/home/angeliki/Documents/cross-situational/corpus/test_version/cds.11_24.images.txt";
-
-        //public static final String VISION_FILE              = "/home/angeliki/Documents/mikolov_composition/misc/pmisvd_more.aggr.dm";
-        public static final String VISION_FILE                = "/home/angeliki/sas/visLang/cross-situational/visual_symbols/all/fc7.txt";
-
-        public static final long SEED                       = 292626718599866L;
-        public static final String TRAIN_CONCEPTS              = "";      
-        public static final String MODEL_FILE               = null;
-        public static final String VECTOR_FILE              = "/home/angeliki/Documents/cross-situational/experiments/vectors/cds"+typeOfLearning
-                                                                                                                    +"_n"+negative_samples+"_m"+margin+typeOfLearning+"_"
-                                                                                                                    +negative_samples+"_r1"+rate_multiplier_sft+
-                                                                                                                    "_r2"+rate_multiplier_grad+"l"+lambda+".bin";                                                                                                                                                            
-        public static final String MAPPING_FUNCTION         = "/home/angeliki/Documents/cross-situational/experiments/mapping/cds"+typeOfLearning
-                                                                                                                        +"_n"+negative_samples+"_m"+margin+typeOfLearning+"_"
-                                                                                                                        +negative_samples+"_r1"+rate_multiplier_sft+
-                                                                                                                        "_r2"+rate_multiplier_grad+"l"+lambda+".mapping"; 
         
-        
-        public static final String VOCABULARY_FILE          = "/home/angeliki/Documents/cross-situational/experiments/vocab/cds"+typeOfLearning+".voc";
-        public static final String INITIALIZATION_FILE      = "/home/angeliki/Documents/cross-situational/experiments/init/cds"+typeOfLearning+".ini";
-        public static final String IMAGE_INITIALIZATION_FILE = "/home/angeliki/Documents/cross-situational/experiments/init/cds.im"+TestConstants.wordDimensions+"_"+TestConstants.imageDimensions+".ini";
-    
-        public static final String LOG_FILE               = "/home/angeliki/Documents/cross-situational/experiments/logs/dump.log";
-        public static final String LOG_DIR               = "/home/angeliki/Documents/cross-situational/experiments/logs";
-        
+        //parallel data
+        public static final String SOURCE_FILE                  = ROOT_EXP_DIR+"corpus/test_version/cds.11_24.words.txt";
+        public static final String TARGET_FILE                  = ROOT_EXP_DIR+"corpus/test_version/cds.11_24.images.txt";
         public static final String TRAIN_DIR               = "";
-        public static final String MEN_FILE             = "/home/angeliki/Documents/mikolov_composition/misc/MEN_dataset_lemma_nopos_form_full";
+
+
+        //visual vectors
+        public static final String VISION_FILE                = ROOT_VISUAL_DIR+"fc7.txt";
+
+        public static final String TRAIN_CONCEPTS              = "";   
         
-              
+        //model files
+        public static final String MODEL_FILE               = null;
         
-        public static final String SIMLEX_FILE             = "/home/angeliki/Documents/mikolov_composition/misc/simlex-999.full";
-        public static final String Carina_FILE             = "/home/angeliki/Documents/mikolov_composition/misc/similarity_judgements3.txt";
-        public static final String CCG_MEN_FILE             = "/home/angeliki/Documents/mikolov_composition/misc/MEN_dataset_lemma_nopos_form_full";
-        public static final String CCG_AN_FILE              = "/home/thenghiapham/work/project/mikolov/an_ml/an_ml_lemmapos.txt";
+        public static final String VECTOR_FILE              =  ROOT_EXP_DIR+"experiments/vectors/cds"+"_n"+negative_samples+"_m"+margin+"_"
+                                                                                                      +"_r1"+rate_multiplier_sft
+                                                                                                      +"_r2"+rate_multiplier_grad+"l"+lambda+".bin";                                                                                                                                                            
+        public static final String MAPPING_FUNCTION         = ROOT_EXP_DIR+"experiments/vectors/cds"+"_n"+negative_samples+"_m"+margin+"_"
+                                                                                                        +"_r1"+rate_multiplier_sft
+                                                                                                        +"_r2"+rate_multiplier_grad+"l"+lambda+".mapping"; 
+        
+        
+        public static final String VOCABULARY_FILE          = ROOT_EXP_DIR+"experiments/vocab/cds"+".voc";
+        public static final String INITIALIZATION_FILE      = ROOT_EXP_DIR+"experiments/init/cds"+".ini";
+        public static final String IMAGE_INITIALIZATION_FILE = ROOT_EXP_DIR+"experiments/init/cds.im"+TestConstants.wordDimensions+"_"+TestConstants.imageDimensions+".ini";
+    
+        public static final String LOG_FILE               = ROOT_EXP_DIR+"experiments/logs/dump.log";
+        public static final String LOG_DIR               = ROOT_EXP_DIR+"experiments/logs";
+        
+        
+        //eval files
+        public static final String MEN_FILE             = ROOT_EVAL_DIR+"MEN_dataset_lemma_nopos_form_full";
+        public static final String SIMLEX_FILE          = ROOT_EVAL_DIR+"simlex-999.full";
+        public static final String Carina_FILE          = ROOT_EVAL_DIR+"similarity_judgements3.txt";
+        public static final String CCG_MEN_FILE         = ROOT_EVAL_DIR+"MEN_dataset_lemma_nopos_form_full";
+        public static final String CCG_AN_FILE          = ROOT_EVAL_DIR+"an_ml/an_ml_lemmapos.txt";
 
   
 //        //for cluster
